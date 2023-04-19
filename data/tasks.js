94a275cabca113389fb99c03c9cc741a68ae9821
Vishal Verma <vishalvrm99@gmail.com>
2023-04-19 22:28:09 +0530

    files added

---
+++ data/tasks.js
@@ -0,0 +1,20 @@
+{
+    tasks:[
+        {
+           title: "Chrome",
+           description: "Web Browser", 
+           isCompleted: false,
+           id:1,
+           created_at: "2023-04-14",
+           priority: "low"
+        },
+        {
+            title: "File Manager",
+            description: "manages files and folders", 
+            isCompleted: false,
+            id:2,
+            created_at: "2023-04-15",
+            priority: "medium"
+         },
+    ]
+}
