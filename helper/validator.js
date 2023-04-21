a89ab5ff4dab0927a0aac46aa2bd434aaf2c0b12
Vishal Verma <vishalvrm99@gmail.com>
2023-04-21 21:49:38 +0530

    Post | Put | Delete API Added

---
+++ helper/validator.js
@@ -0,0 +1,40 @@
+class validator {
+    static validateTaskInfo(taskInfo, taskData) {
+      if(taskInfo.hasOwnProperty("title") &&
+        taskInfo.hasOwnProperty("description") &&
+        taskInfo.hasOwnProperty("isCompleted") &&
+        taskInfo.hasOwnProperty("id") && 
+        taskInfo.hasOwnProperty("priority") && 
+        this.validateUniqueTaskId(taskInfo, taskData)) {
+          return {
+            "status": true,
+            "message": "Task has been added"
+          };
+        }
+        if(!this.validateUniqueTaskId(taskInfo, taskData)){
+          return {
+            "status": false,
+            "message": "Task id has to be unique"
+          };
+        }
+        return {
+          "status": false,
+          "message": "Task Info is malformed please provide all the properties"
+        }
+    }
+  
+    static validateUniqueTaskId(taskInfo, taskData) {
+      let valueFound = taskData.tasks.some(el => el.id === taskInfo.id);
+      if(valueFound) return false;
+      return true;
+    }
+  
+    static validateIsCompleted(taskInfo) {
+      if(taskInfo.hasOwnProperty("isCompleted") && typeof taskInfo['isCompleted'] == "boolean") {
+        return true;
+      }
+      return false;
+    }
+  }
+  
+  module.exports = validator;
--- routes/taskInfo.js
+++ routes/taskInfo.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const validator = require("../helper/validator");
 //Tasks Data
 const tasksData = require("../tasks.json");
 
@@ -12,27 +12,26 @@ const routes = express.Router();
 //get all tasks
 routes.get("/", (req, res) => {
   try {
-    let {sortBy , orderBy, isCompleted} = req.query;
+    let { sortBy, orderBy, isCompleted } = req.query;
     let airtribeTasks = tasksData.tasks;
 
-    if(sortBy){
-        if(orderBy == 'desc'){
-            airtribeTasks = airtribeTasks.sort((task1,task2)=>{
-                let d1 = new Date(task1.created_at);
-                let d2 = new Date(task2.created_at);
-                return d1.getTime() > d2.getTime();
-            })
-        }else{
-            airtribeTasks = airtribeTasks.sort((task1,task2)=>{
-                let d1 = new Date(task1.created_at);
-                let d2 = new Date(task2.created_at);
-                return d1.getTime() < d2.getTime();
-            })
-        }
+    if (sortBy) {
+      if (orderBy == "desc") {
+        airtribeTasks = airtribeTasks.sort((task1, task2) => {
+          let d1 = new Date(task1.created_at);
+          let d2 = new Date(task2.created_at);
+          return d1.getTime() > d2.getTime();
+        });
+      } else {
+        airtribeTasks = airtribeTasks.sort((task1, task2) => {
+          let d1 = new Date(task1.created_at);
+          let d2 = new Date(task2.created_at);
+          return d1.getTime() < d2.getTime();
+        });
+      }
     }
-
     res.status(200);
-    res.send(tasksData);
+    res.send({ tasks: airtribeTasks });
   } catch (err) {
     res.status(500);
     res.send({ message: "Something went wrong" });
@@ -48,6 +47,99 @@ routes.get("/:taskId", (req, res) => {
     if (result) {
       res.status(200);
       res.send(result);
+    } else {
+      res.status(404);
+      res.send({ message: "task not found" });
+    }
+  } catch (err) {
+    res.status(500);
+    res.send({ message: "Something went wrong" });
+  }
+});
+
+//create a task
+routes.post("/", (req, res) => {
+  try {
+    const taskDetails = req.body;
+    let writePath = path.join(__dirname, "../data", "tasks.json");
+    if (validator.validateTaskInfo(taskDetails, tasksData).status) {
+      let tempTaskData = { ...tasksData };
+      tempTaskData.tasks.push(taskDetails);
+      fs.writeFileSync(writePath, JSON.stringify(tempTaskData), {
+        encoding: "utf8",
+        flag: "w",
+      });
+      res.status(200);
+      res.json(validator.validateCourseInfo(taskDetails, tasksData));
+    } else {
+      res.status(400);
+      res.json(validator.validateTaskInfo(taskDetails, tasksData));
+    }
+  } catch (err) {
+    res.status(500);
+    res.send({ message: "Something went wrong" });
+  }
+});
+
+//update a task
+routes.post("/:taskId", (req, res) => {
+  try {
+    const taskDetails = req.body;
+    const { taskId } = req.params;
+    let writePath = path.join(__dirname, "../data", "tasks.json");
+    if (validator.validateTaskInfo(taskDetails, tasksData).status) {
+      let tempTaskData = { ...tasksData };
+      let isFound = false;
+      tempTaskData.tasks = tempTaskData.tasks.map((item) => {
+        if (item.id == taskId) {
+          isFound = true;
+          return { ...taskDetails };
+        }
+        return item;
+      });
+      if(isFound){
+        fs.writeFileSync(writePath, JSON.stringify(tempTaskData), {
+          encoding: "utf8",
+          flag: "w",
+        });
+        res.status(200);
+        res.send({ message: "task Updated successfully" });
+      }else{
+        res.status(404);
+        res.send({ message: "task not found" });
+      }
+    } else {
+      res.status(400);
+      res.json(validator.validateTaskInfo(taskDetails, tasksData));
+    }
+  } catch (err) {
+    res.status(500);
+    res.send({ message: "Something went wrong" });
+  }
+});
+
+//Delete a task
+routes.delete("/:taskId", (req, res) => {
+  try {
+    const { taskId } = req.params;
+    let writePath = path.join(__dirname, "../data", "tasks.json");
+    let tempTaskData = { ...tasksData };
+
+    let isTaskFound = false;
+    tempTaskData.task = tempTaskData.task.filter((item) => {
+      if (item.id == taskId) {
+        isTaskFound = true;
+        return false;
+      }
+      return true;
+    });
+    if (isTaskFound) {
+      fs.writeFileSync(writePath, JSON.stringify(tempTaskData), {
+        encoding: "utf8",
+        flag: "w",
+      });
+      res.status(200);
+      res.send({ message: "task deleted successfully" });
     } else {
       res.status(404);
       res.send({ message: "task not found" });
